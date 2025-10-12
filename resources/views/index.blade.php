61b50846e6949ccf638b3e11dfa88af0f6e2856d
Ana Ifantri <ana.ifantri@gmail.com>
2025-10-12 20:53:31 +0800

    Memperbaiki fitur daya, pembayaran dan pengisian pulsa listrik

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- @vite('resources/css/app.css') --}}
-    <title>VISTA MEDIA | {{ $title }}</title>
+    <title>SARANA MEDIA PERSADA | {{ $title }}</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="icon" href="favicon.ico" type="image/x-icon">
 </head>
